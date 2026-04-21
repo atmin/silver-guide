3594e68bea92406192393eed68bbc435028c907d
Atanas Minev <atmin@protonmail.com>
2026-04-21 11:13:47 +0300

    feat: product CRUD API

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -8,7 +8,32 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ['id', 'name', 'slug', 'parent', 'children']
+        fields = ["id", "name", "slug", "parent", "children"]
 
     def get_children(self, obj):
         return CategorySerializer(obj.children.all(), many=True).data
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        source="category",
+        write_only=True,
+    )
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "sku",
+            "title",
+            "description",
+            "image",
+            "price",
+            "category",
+            "category_id",
+            "is_active",
+            "created_at",
+            "updated_at",
+        ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,22 @@
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        return Category.objects.select_related('parent').prefetch_related('children')
+        return Category.objects.select_related("parent").prefetch_related("children")
+
+
+class ProductViewSet(ModelViewSet):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.filter(is_active=True).select_related("category")
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
