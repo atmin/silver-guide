c5c03cb6038faa11c4413fe3a152b661a39704f8
Atanas Minev <atmin@protonmail.com>
2026-04-21 15:08:27 +0300

    docs: clarify init.sql

--- docker/mariadb/init.sql
+++ docker/mariadb/init.sql
@@ -1,2 +1,5 @@
+-- The MariaDB image only grants the app user access to MYSQL_DATABASE.
+-- pytest-django needs to CREATE/DROP a separate test_catalog database,
+-- which requires an explicit grant here. No Docker env var covers this.
 GRANT ALL ON `test_catalog`.* TO 'catalog'@'%';
 FLUSH PRIVILEGES;
